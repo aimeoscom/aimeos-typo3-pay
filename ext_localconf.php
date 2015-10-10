963cfb512f45d22993b6165e63f67dc3d43c6c5a
Aimeos <aimeos@aimeos.org>
2015-10-10 20:03:31 +0200

    Package update

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,10 @@ if ( ! defined( 'TYPO3_MODE' ) ) {
 	die ( 'Access denied.' );
 }
 
+
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath( $_EXTKEY ) . '/Resources/Libraries/autoload.php';
+
+
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['aimeos']['extDirs']['1_'.$_EXTKEY] =
   'EXT:' . $_EXTKEY . '/Resources/Private/Extensions/';
 
